cd53aacc1f60ed8321803445baccb9d977663b52
cjhd92 <cjhd92@gmail.com>
2024-03-22 12:27:29 +0100

    chat completo

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,6 +1,7 @@
 import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
+import Chat from './Chat';
 import './Page.css';
 
 const Page: React.FC = () => {
@@ -9,24 +10,30 @@ const Page: React.FC = () => {
 
   return (
     <IonPage>
-      <IonHeader>
+      {/* <IonHeader>
         <IonToolbar>
           <IonButtons slot="start">
             <IonMenuButton />
           </IonButtons>
           <IonTitle>{name}</IonTitle>
         </IonToolbar>
-      </IonHeader>
+      </IonHeader> */}
 
-      <IonContent fullscreen>
+      {/* <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer name={name} />
-      </IonContent>
+        
+        
+      </IonContent> */}
+
+      <Chat/>
     </IonPage>
+
+    
   );
 };
 
